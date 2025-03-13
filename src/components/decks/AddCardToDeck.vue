94006a25f756bbe2e94be76f08b0ec89b7a5fa88
Cyprien <cyprien.forge@gmail.com>
2025-03-13 18:22:52 +0100

    :sparkles: - Finish UI for decks and manage doublons

--- src/components/decks/AddCardToDeck.vue
+++ src/components/decks/AddCardToDeck.vue
@@ -4,6 +4,7 @@ import DeckService from "@/services/DeckService.js";
 import {onMounted, ref} from "vue";
 
 const decks = ref()
+const result = ref(true)
 const deckService = new DeckService()
 
 const props = defineProps({
@@ -17,9 +18,10 @@ onMounted(async () => {
 
 const selectedDeck = defineModel()
 
-function addCardToDeck(event){
+async function addCardToDeck(event){
+  result.value = true
   const deckService = new DeckService()
-  deckService.addCard(selectedDeck.value, props.id)
+  result.value = await deckService.addCard(selectedDeck.value, props.id)
 }
 
 </script>
@@ -33,8 +35,22 @@ function addCardToDeck(event){
     </select>
     <input type="submit" value="Add to Deck" />
   </form>
+
+  <p v-if="!result" :class="{error: !result}">
+    Cette carte est déjà dans ce deck !
+  </p>
+
 </template>
 
 <style scoped>
+
+.error{
+  color:red;
+}
+
+p{
+  font-size: 0.75rem;
+  padding-bottom: 2%;
+}
 
 </style>

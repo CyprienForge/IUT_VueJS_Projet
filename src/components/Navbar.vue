91d22722b365e0bc3fe0f3ae3025073b85d87564
louis.guichard-montguers <louis.guichard-montguers@etu.uca.fr>
2025-03-15 11:59:48 +0100

    ajout icones nav bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,26 +8,64 @@ const links = linkService.getLinks()
 </script>
 
 <template>
-
-  <div>
-    <ul v-for="link in links" style="display: inline-block">
-      <RouterLink :to="`${link.path}`">
-        {{ link.label }}
-      </RouterLink>
+  <div class="navbar">
+    <ul>
+      <li v-for="link in links" :key="link.path">
+        <RouterLink :to="link.path" class="nav-link">
+          <img :src="link.icon" :alt="link.label" class="nav-icon" :class="{ small: link.label === 'My Deck' }" />
+        </RouterLink>
+      </li>
     </ul>
   </div>
-
 </template>
 
 <style scoped>
-div{
+.navbar {
   width: 100%;
   background-color: rgb(250, 68, 68);
   display: flex;
-  justify-content: space-evenly;
+  align-items: center;  /* Assure un alignement vertical des éléments */
   height: 6%;
-  align-items: center;
-  text-decoration: none;
-  cursor: pointer;
+  padding: 0 20px;
 }
+
+ul {
+  display: flex;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  justify-content: space-evenly; /* Espacement des icônes */
+  width: 100%;
+}
+
+li {
+  margin: 0 10px;
+  display: flex;
+
+  align-items: center;  /* Centre l'icône verticalement */
+}
+
+.nav-link {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+}
+
+.nav-icon {
+  width: 48px;  /* Taille par défaut */
+  height: 48px; /* Taille par défaut */
+  transition: transform 0.2s ease, filter 0.3s;
+}
+
+.nav-icon:hover {
+  transform: scale(1.2);
+  filter: brightness(1.2);
+}
+
+.nav-icon.small {
+  width: 70px;  /* Taille spécifique pour "Home" */
+  height: 70px; /* Taille spécifique pour "Home" */
+}
+
 </style>
--- src/services/LinkService.js
+++ src/services/LinkService.js
@@ -2,13 +2,12 @@ export default class LinkService {
 
     getLinks(){
 
-        const links = [
-            { label: 'Home', path: '/' },
-            { label: 'All Cards', path: '/all' },
-            { label: 'My Deck', path: '/deck' },
-            { label: 'Open a booster', path: '/open' }
+        return [
+            {label: "Home", path: "/", icon: "../../public/home.png"},
+            {label: "All Cards", path: "/all", icon: "../../public/cards.webp"},
+            {label: "My Deck", path: "/deck", icon: "../../public/deck.webp"},
+            {label: "Open a booster", path: "/open", icon: "../../public/boosterOpen.png"}
         ]
-        return links
     }
 
 }
